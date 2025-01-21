524bcb1200e9778dc76c53850d9af86ecd0768a2
ifnk <975779964@qq.com>
2025-01-21 22:17:21 +0800

    d

--- src/middleware/exception_middleware.rs
+++ src/middleware/exception_middleware.rs
@@ -6,15 +6,25 @@ use axum::response::IntoResponse;
 use crate::util_entity::response::{CusResponse, failed};
 
 pub fn handle_exception(err: Box<dyn Any + Send + 'static>) -> Response<Body> {
-    let details = if let Some(s) = err.downcast_ref::<String>() {
+    let msg = if let Some(s) = err.downcast_ref::<String>() {
         s.clone()
     } else if let Some(s) = err.downcast_ref::<&str>() {
         s.to_string()
     } else {
         "Unknown panic message".to_string()
     };
+    let details = if std::env::var("ENV").unwrap_or_default() == "production" {
+        format!("Error occurred at {}:{}", file!(), line!())
+    } else {
+        format!(
+            "Error occurred at {}:{}\nStack trace: {:?}", 
+            file!(), 
+            line!(),
+            backtrace::Backtrace::new()
+        )
+    };
 
-    let cus_response: CusResponse<()> = failed(details);
+    let cus_response: CusResponse<()> = failed(msg, Some(details));
     let response = Json(cus_response);
 
     response.into_response()
